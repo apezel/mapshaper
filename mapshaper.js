d62252fb73b1434c91c75f1587c5a1f0b083842d
apezel <arnaud.pezel@me.com>
2021-03-25 10:43:55 +0100

    bundle

--- mapshaper.js
+++ mapshaper.js
@@ -18040,7 +18040,7 @@ ${svg}
 
 
   function importCRS(dataset, jsonObj) {
-    if ('crs' in jsonObj && jsonObj.crs.properties && jsonObj.properties.name) {
+    if ('crs' in jsonObj && jsonObj.crs.properties && jsonObj.crs.properties.name) {
       dataset.info.input_geojson_crs = jsonObj.crs;
       dataset.info.crs = getCRS(jsonObj.crs.properties.name);
     }
